ff3d0094982d844cf9f801c58a90c7b4bf2e6e06
shaojiewang <wsjmessi@163.com>
2024-04-18 07:06:29 +0000

    modify the script

--- script/build.sh
+++ script/build.sh
@@ -1,4 +1,14 @@
 rm -rf ./build/*
 
-nvcc gemm_test.cu -o ./build/gemm_test -L/usr/local/cuda/lib64 -lcudart -lcuda -lcublas -std=c++17 -gencode=arch=compute_90,code=sm_90
+if [ $# -eq 2 ]; then
+    SM=$1
+else
+    SM=90
+fi
+
+nvcc gemm_test.cu -o ./build/gemm_test -L/usr/local/cuda/lib64 -lcudart -lcuda -lcublas -std=c++17 -gencode=arch=compute_${SM},code=sm_${SM}
+
+# disassembly
+cuobjdump -sass build/gemm_test > fma_bench.sass
+
 
